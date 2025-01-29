9940eb0e5a38ad659d8315c2a26ab3e7e7638666
Jacek Langer <jacek.langer@outlook.de>
2025-01-29 22:05:17 +0100

    chore: merge

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-jar -cf  libs/PrimePalindrom.jar test
-=======
 jar -cf  libs/PrimesPalindromChecker.jar test
->>>>>>> dev
-java  -cp libs/* src/org/rodias/jl/Entry.java
+java  -cp libs/* src/org/rodias/jl/Entry.java"
